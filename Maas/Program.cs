dde3be6cf62cb7e1cfc24a17e4a1daa67df0c3dd
Subhan V. Azimli <subhanav@code.local>
2020-10-22 15:24:13 +0400

    sd

---
+++ Maas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Maas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TeacherSalary salary = new TeacherSalary();
+
+            salary.AddLesson(5, 10);//150
+            salary.AddLesson(7, 3); //45
+            salary.AddLesson(10, 2);//40
+
+
+            salary.Show();
+            salary.Removelesson(10);
+            
+            Console.WriteLine("Total Salary:" + salary.CalcSalary());
+
+            Console.WriteLine("==================");
+            
+
+        }
+    }
+}
---
+++ Maas/TeacherSalary.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maas
+{
+    class TeacherSalary
+    {
+        private int[] lessonHour = new int[11];
+        private double[] lessonIncome = new Double[11];
+        private double _totalIncome;
+
+        public void AddLesson(int level, int hour)
+        {
+
+            if (level >= 1 && level <= 4)
+            {
+                lessonHour[level - 1] += hour;
+                lessonIncome[level - 1] += hour * 10;
+
+                
+                
+            }
+            else if (level >= 5 && level <= 9)
+            {
+                lessonHour[level - 1] += hour;
+                lessonIncome[level - 1] += hour * 15;
+               
+            }
+            else
+            {
+                lessonHour[level - 1] += hour;
+                lessonIncome[level - 1] += hour * 20;
+                
+            }
+
+            }
+
+            public double CalcSalary()
+            {
+                for (int i = 0; i < 11; i++)
+                {
+                    _totalIncome += lessonIncome[i];
+                }
+                return _totalIncome;
+
+
+            }
+        public void Show()
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                if (lessonHour[i]!=0)
+                {
+                    Console.WriteLine($"{i + 1} sinfe {lessonHour[i]} saat ders deyilib");
+                }
+                
+            }
+
+
+        }
+        public void Removelesson(int lesson)
+            {
+               
+                _totalIncome = _totalIncome - lessonIncome[lesson - 1];
+               
+
+
+
+            }
+       
+
+         
+        }
+    } 
+       
+        
+        
+
+
+    
